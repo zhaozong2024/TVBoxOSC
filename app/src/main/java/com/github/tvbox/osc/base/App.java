1de2745d219e78d2455417b5b82aae857065cc5c
zhaozong2024 <158453588+zhaozong2024@users.noreply.github.com>
2024-02-13 07:32:40 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -53,6 +53,16 @@ public class App extends MultiDexApplication {
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
             Hawk.put(HawkConfig.PLAY_TYPE, 1);
         }
+        //自定义默认配置，硬解，安全dns，缩略图
+       if (!Hawk.contains(HawkConfig.IJK_CODEC)) {
+           Hawk.put(HawkConfig.IJK_CODEC, "硬解码");        
+       } 
+       if (!Hawk.contains(HawkConfig.DOH_URL)) {
+           Hawk.put(HawkConfig.DOH_URL, 2);        
+       }
+       if (!Hawk.contains(HawkConfig.SEARCH_VIEW)) {
+           Hawk.put(HawkConfig.SEARCH_VIEW, 2);        
+       } 
     }
 
     public static App getInstance() {
