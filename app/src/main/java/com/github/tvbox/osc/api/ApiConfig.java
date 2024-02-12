fc9408de3a79183d6dc4318e3c30f3f559b4cba7
zhaozong2024 <158453588+zhaozong2024@users.noreply.github.com>
2024-02-13 07:25:10 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://xingba.gd2.qingstor.com/9.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
